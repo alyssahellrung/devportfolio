06051b0a6e66fa6bbe670d99029d8e0e62ed30d4
alyssahellrung <alyssahellrung@gmail.com>
2020-07-25 15:00:32 -0700

    Updates

---
+++ precache-manifest.f2d1729d247c89245ca9d3944bfdffe9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d15a5a21a0791ffab0820e77011648",
+    "url": "/devportfolio/index.html"
+  },
+  {
+    "revision": "c183c17ddb41acf1112a",
+    "url": "/devportfolio/static/css/main.291a06b4.chunk.css"
+  },
+  {
+    "revision": "19ffdc91f19255f5d7f2",
+    "url": "/devportfolio/static/js/2.53d40ebb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/devportfolio/static/js/2.53d40ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c183c17ddb41acf1112a",
+    "url": "/devportfolio/static/js/main.28ba0b70.chunk.js"
+  },
+  {
+    "revision": "dbb2b16238a2ba7a0b6e",
+    "url": "/devportfolio/static/js/runtime-main.d6b523f9.js"
+  },
+  {
+    "revision": "00b22e9ea40e3b3fb5a02ea48f4e7961",
+    "url": "/devportfolio/static/media/seigaiha.00b22e9e.png"
+  }
+]);
